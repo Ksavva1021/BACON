bdd02aa074cc6203febebd01eeaaed02136437c7
Klitos <kleitos.savva.97@gmail.com>
2024-11-13 11:07:48 +0100

    Updates

--- create_jsons.py
+++ create_jsons.py
@@ -8,9 +8,9 @@ def create_jsons(args):
     with open(f'samples/{args.year}.yaml') as f:
         samples = yaml.load(f, Loader=yaml.FullLoader)
 
-    subprocess.run(['gfal-mkdir', f'{args.destination_path}/Run3/{args.year}'])
+    subprocess.run(['gfal-mkdir', f'{args.destination_path}/{args.year}'])
     for sample in samples:
-        subprocess.run(['gfal-mkdir', f'{args.destination_path}/Run3/{args.year}/{sample}'])
+        subprocess.run(['gfal-mkdir', f'{args.destination_path}/{args.year}/{sample}'])
 
     for sample in samples:
         source_sample_path = f'{args.source_path}/{args.year}/{sample}'
@@ -25,7 +25,7 @@ def create_jsons(args):
         for file in files:
             file_entry = {
                 "sources": [f'{source_sample_path}/{file}'],
-                "destinations": [f'{args.destination_path}/Run3/{args.year}/{sample}/{file}']
+                "destinations": [f'{args.destination_path}/{args.year}/{sample}/{file}']
             }
             data["files"].append(file_entry)
 
@@ -37,7 +37,7 @@ def create_jsons(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create JSONs from YAMLs')
     parser.add_argument('--year', required=True, help='Year to process')
-    parser.add_argument('--destination_path', required=False, help='Path to create directories on dcache', default='davs://gfe02.grid.hep.ph.ic.ac.uk:2880/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/ksavva/HLepRare_skim_2024_v2')
+    parser.add_argument('--destination_path', required=False, help='Path to create directories on dcache', default='davs://gfe02.grid.hep.ph.ic.ac.uk:2880/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/ksavva/HLepRare/skim_2024_v2')
     parser.add_argument('--source_path', required=False, help='Path to sample root files', default='davs://eoscms.cern.ch/eos/cms/store/group/phys_higgs/HLepRare/skim_2024_v2')
     args = parser.parse_args()
 
